e5a648bd8a2ff58a83303110d7ccb9f1dece1c63
Dmitry <107542907+1nkret@users.noreply.github.com>
2024-09-19 23:56:48 +0300

    notify to all from owner

---
+++ Bot/apps/broadcast/handlers.py
@@ -0,0 +1,40 @@
+from aiogram import Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import Command
+
+from Bot.apps.broadcast.forms import Broadcast
+from Bot.apps.broadcast.keyboards import broadcast_cancel
+from Bot.apps.menu.keyboard import get_main_menu
+
+from Bot.core.config import bot
+from Bot.utils.access.members import get_from_json_members
+
+router = Router()
+
+
+@router.message(Command("broadcast"))
+async def broadcast_handler(event: Message, state: FSMContext):
+    await event.answer(
+        text="Type your message to alert all:",
+        reply_markup=broadcast_cancel()
+    )
+    await state.set_state(Broadcast.message)
+
+
+@router.message(Broadcast.message)
+async def broadcast_end_handler(event: Message, state: FSMContext):
+    await event.answer("Successful.")
+    await state.clear()
+    for aci in get_from_json_members():
+        await bot.send_message(
+            chat_id=aci,
+            text=event.text
+        )
+
+
+@router.callback_query(lambda c: c.data == "broadcast_cancel")
+async def broadcast_cancel_handler(event: CallbackQuery, state: FSMContext):
+    await state.clear()
+    await event.message.answer("Canceled.",
+                               reply_markup=get_main_menu(event.chat.id))
